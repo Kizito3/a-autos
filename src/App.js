267f869190f0c98f7e8cf205c9df9a46a473255c
unknown <chigokizzy@gmail.com>
2023-05-12 02:53:11 +0100

    header done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+// import '../src../style/global.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
